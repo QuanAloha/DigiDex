4e0b6048bc289aba0ad202976e3754ffd99b81bf
QuanAloha <nathaniel.paul.2018@icloud.com>
2022-03-28 11:57:55 -0600

    Cleaned Up Code

--- src/digiDexMain/Phone.java
+++ src/digiDexMain/Phone.java
@@ -2,16 +2,14 @@ package digiDexMain;
 
 public class Phone {
     
+    // FIELDS
     private String employeeFirstName;
-
-    private String employeeLastName;
-    
+    private String employeeLastName; 
     private long phoneNumber;
-
     private int phoneAssetCode;
-
     private int routeNumber;
 
+    // CONSTRUCTOR
     public Phone (String employeeFirstName, String employeeLastName, int phoneAssetCode, int routeNumber) {
 
         this.employeeFirstName = employeeFirstName;
@@ -25,45 +23,25 @@ public class Phone {
         // Set a phone number for the object based on what phone asset code is provided. 
 
         if (phoneAssetCode == 123) {
-
             this.phoneNumber = 1234567890L;
-
         } else if (phoneAssetCode == 321) {
-
             this.phoneNumber = 9376543421L;
-
         } else if (phoneAssetCode == 123) {
-
             this.phoneNumber = 6549874321L;
-
-        } 
-
-        
+        }       
     }
-
 
     // ACCESSOR METHODS
-
     public String getEmployeeName() { return employeeFirstName + " " + employeeLastName; }
-
     public String getEmployeeFirstName() { return employeeFirstName; }
-
     public String getEmployeeLastName() { return employeeLastName; }
-
     public int getRouteNumber() { return routeNumber; }
-
     public int getPhoneAssetCode() { return phoneAssetCode; }
-
     public long getPhoneNumber() { return phoneNumber; }
 
-
-
-
+    // TO STRING METHOD
     public String toString() {
-
         return employeeFirstName + " " + employeeLastName + " on route " + routeNumber + " has phone " + phoneAssetCode + " with the " + phoneNumber + " phone number.";
-
     }
-
 
 }
